2a94e4bb346fc76e86339ef48a1e5a8539aa664b
satyajeetramnit <1906130@kiit.ac.in>
2022-07-23 22:49:18 +0530

    Method 2 for Longest Special Subsequences Added

--- MakeMyTrip/longestSubsequence.cpp
+++ MakeMyTrip/longestSubsequence.cpp
@@ -22,32 +22,38 @@
 // Explaination :
 // One of the longest special subsequence present in afcbedf is a,c,b,d. It is special because |a-c|<=2 , |c-b|<=2 and |b-d|<=2.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int longest_special_subseq(int n, int k, string s){
+int longest_special_subseq(int n, int k, string s)
+{
     vector<int> dp(n, 0);
     int max_length[26] = {0};
- 
-    for (int i = 0; i < n; i++){
+
+    for (int i = 0; i < n; i++)
+    {
         int curr = s[i] - 'a';
         int lower = max(0, curr - k);
         int upper = min(25, curr + k);
-        for (int j = lower; j < upper + 1; j++){
+        for (int j = lower; j < upper + 1; j++)
+        {
             dp[i] = max(dp[i], max_length[j] + 1);
         }
         max_length[curr] = max(dp[i], max_length[curr]);
     }
- 
+
     int ans = 0;
-    for(int i:dp) ans = max(i, ans);
+    for (int i : dp)
+        ans = max(i, ans);
     return ans;
 }
 
-int main(){
+int main()
+{
     int t, n, k;
     cin >> t;
-    while(t--){
+    while (t--)
+    {
         cin >> n >> k;
         string s;
         cin >> s;
@@ -55,9 +61,41 @@ int main(){
     }
 }
 
-// Sample Input: 
+// Sample Input:
 // 1
 // 7 2 afcbedg
 
 // Sample Output:
 // 4
+
+
+// Method 2:
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestSubSeq(string str, int k)
+{
+    int maxx = 0;
+    int temp = 0;
+
+    for (int i = 0; i < str.length() - 1; i++)
+    {
+        if ((int(str[i + 1]) - int(str[i])) <= k)
+        {
+            temp++;
+            maxx = max(temp, maxx);
+        }
+        else
+        {
+            temp = 0;
+        }
+    }
+    cout << "longest substring = " << maxx + 1;
+    return maxx;
+}
+
+int main()
+{
+    longestSubSeq("afcbedf", 2);
+    return 0;
+}
